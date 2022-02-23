dc7f7d93a49efc848a6927d216b0ada302a72c0d
ZSchweyk <zeynschweyk@dpengineering.org>
2022-02-22 17:35:25 -0800

    adjusted inequality to prepare for upgraded bullets

--- ball.py
+++ ball.py
@@ -24,7 +24,7 @@ class Ball(Widget):
                 if isinstance(e, Bullet):
                     e.collision_detected = True
                     self.strength -= 1
-                    if self.strength == 0:
+                    if self.strength <= 0:
                         self.collision_detected = True
                         # The return statement below is not technically needed; it just prevents the computation below.
                         return False
